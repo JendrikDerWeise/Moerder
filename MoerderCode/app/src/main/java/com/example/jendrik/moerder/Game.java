adcc70117cb953a74e500b7af2f45c59b55ae1a1
sophiaaez <me@sophiaaezell.com>
2016-02-24 20:45:26 +0100

    Added files via upload
    Ich habe alles mal schön nach dem Alphabet sortiert, damit man Methoden leichter findet
    Kartenverteilen implementiert

--- MoerderCode/app/src/main/java/com/example/jendrik/moerder/Game.java
+++ MoerderCode/app/src/main/java/com/example/jendrik/moerder/Game.java
@@ -16,11 +16,11 @@ import java.util.Random;
  *
  */
 public class Game implements Serializable {
-    private Solution solution;
-    private RoomManager roomManager;
-    private WeaponManager weaponManager;
-    private PlayerManager playerManager;
-    private ArrayList<Card> cardList;
+    private Solution solution = null;
+    private RoomManager roomManager = new RoomManager();
+    private WeaponManager weaponManager = new WeaponManager();
+    private PlayerManager playerManager = new PlayerManager();
+    private ArrayList<Card> cardList = new ArrayList<Card>();
 
     public Game(ArrayList<String> rooms, ArrayList<String> weapons){
         createRooms(rooms);
@@ -28,33 +28,6 @@ public class Game implements Serializable {
     }
 
     public Game(){} //nur für das Laden verwendet
-
-    public void startGame(ArrayList<String> players){
-        createPlayer(players);
-        createCards();
-        giveCardsToPlayer();
-        //Spiel Speichern über GUI
-        //Auslöser zum Senden des Savegames -->gehört in ServerClass
-    }
-
-    private void createPlayer(ArrayList<String> players){
-        for(String s:players)
-            playerManager.addPlayer(s);
-    }
-
-    private void createRooms(ArrayList<String> rooms){
-        for (String s:rooms)
-            roomManager.createRoom(s);
-    }
-
-    private void createWeapons(ArrayList<String> weapons){
-        for (String s:weapons)
-            weaponManager.createWeapon(s);
-
-        for(int i=0; i<weaponManager.getWeaponList().size();i++){ //Zuteilung der Waffen auf die Räume. Vorraussetzung:  Anzahl Waffen <=> Anzahl Räume
-            roomManager.showMap().get(i).addWeapon(weaponManager.getWeaponList().get(i));
-        }
-    }
 
     private void createCards(){
         for (Player p:playerManager.getPlayerList())
@@ -69,46 +42,92 @@ public class Game implements Serializable {
         createSolution();
     }
 
-    private void createSolution(){
+    private void createPlayer(ArrayList<String> players){
+        for(String s:players)
+            playerManager.addPlayer(s);
+    }
 
+    private void createSolution(){
         int playerCount = playerManager.getPlayerList().size();
         int roomCount = roomManager.showMap().size();
         int weaponCount = weaponManager.getWeaponList().size();
         Random random = new Random();
         String pName = playerManager.getPlayerList().get(random.nextInt(playerCount)).getName();
-        String rName = roomManager.showMap().get(random.nextInt(((playerCount+roomCount) - playerCount) + playerCount)).getName();
-        String wName = weaponManager.getWeaponList().get(random.nextInt(((roomCount+playerCount+weaponCount) - roomCount) + roomCount)).getName();
+        String rName = roomManager.showMap().get(random.nextInt(roomCount)).getName();
+        String wName = weaponManager.getWeaponList().get(random.nextInt(weaponCount)).getName();
         solution = new Solution(pName, rName,wName);
     }
 
-    public boolean compairSolution(String murder, String room, String weapon){
-        if(solution.getMurder()==murder && solution.getRoom()==room && solution.getWeapon()==weapon)
+    private void createRooms(ArrayList<String> rooms){
+        for (String s:rooms)
+            roomManager.createRoom(s);
+    }
+
+    private void createWeapons(ArrayList<String> weapons){
+        for (String s:weapons)
+            weaponManager.createWeapon(s);
+
+        for (int i = 0; i < weaponManager.getWeaponList().size();i++){ //Zuteilung der Waffen auf die Räume. Vorraussetzung:  Anzahl Waffen <=> Anzahl Räume
+            roomManager.showMap().get(i).addWeapon(weaponManager.getWeaponList().get(i));
+        }
+    }
+
+    public boolean compareSolution(String murderer, String room, String weapon){
+        if(solution != null && solution.getMurderer()==murderer && solution.getRoom()==room && solution.getWeapon()==weapon)
             return true;
         else
             return false;
-    }
-
-    public void killPlayer(int pNumber){
-        playerManager.getPlayerList().get(pNumber).setDead(true);
-    }
-
-    private void giveCardsToPlayer(){
-        int i=0;
-        int playerCount = playerManager.getPlayerList().size();
-        for (Card c:cardList
-             ) {
-            playerManager.giveCard(c, i);
-            if(i+1==playerCount)
-                i=0;
-            else
-                i++;
-        }
     }
 
     public ArrayList<Room> getRooms(){
         return roomManager.showMap();
     }
 
+    private void giveCardsToPlayer(){
+        ArrayList<Card> copyCardList = new ArrayList<Card>();//duplicates Cardlist
+        for(int i = 0; i < cardList.size(); i++){
+            copyCardList.add(i, cardList.get(i));
+        }
 
+        int playerCount = playerManager.getPlayerList().size();
+        Random random = new Random();
+        int cardPosition = 0;
+        boolean given = false;
+        while(!copyCardList.isEmpty()){ //until copied List is not Empty
+            for(int i = 0; i < playerCount; i++){
+                while(!given) {
+                    cardPosition = random.nextInt(copyCardList.size());
+                    given = false;
+                    if (copyCardList.get(cardPosition).getName() == solution.getMurderer() ||
+                            copyCardList.get(cardPosition).getName() == solution.getWeapon() ||
+                            copyCardList.get(cardPosition).getName() == solution.getRoom()) {
+                        //when the card is part of the solution
+                        //card is deleted from Copied List
+                        copyCardList.remove(cardPosition);
+                        //loop repeats
+                    }else { //if card is not part of solution
+                        //card is given
+                        playerManager.giveCard(copyCardList.get(cardPosition), i);
+                        given = true;
+                        //Card is removed from copied List
+                        copyCardList.remove(cardPosition);
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void killPlayer(int pNumber){
+        playerManager.getPlayerList().get(pNumber).setDead(true);
+    }
+
+    public void startGame(ArrayList<String> players){
+        createPlayer(players);
+        createCards();
+        giveCardsToPlayer();
+        //Spiel Speichern über GUI
+        //Auslöser zum Senden des Savegames -->gehört in ServerClass
+    }
 
 }
