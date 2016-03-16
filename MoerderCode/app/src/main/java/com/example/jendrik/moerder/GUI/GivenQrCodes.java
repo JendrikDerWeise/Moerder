dc7adcb502c8d546a276951154695cdacffb7e64
JendrikDerWeise <jbulk@stud.hs-bremen.de>
2016-03-16 14:38:27 +0100

    Kommentare für GivenQrCodes

--- MoerderCode/app/src/main/java/com/example/jendrik/moerder/GUI/GivenQrCodes.java
+++ MoerderCode/app/src/main/java/com/example/jendrik/moerder/GUI/GivenQrCodes.java
@@ -30,28 +30,28 @@ public class GivenQrCodes extends Activity {
 
 
 
-        for(Room r : game.getRooms()) {
+        for(Room r : game.getRooms()) { //create table for room- and weaponnames & QR-Codes
             TableRow row = new TableRow(this);
             TableRow row2 = new TableRow(this);
-            TextView tv_roomName = new TextView(this);
+            TextView tv_roomName = new TextView(this); //fill table with TextViews
             TextView tv_qrCodeR = new TextView(this);
             TextView tv_weaponName = new TextView(this);
             TextView tv_qrCodeW = new TextView(this);
 
-            tv_roomName.setText("Raumname: " + r.getName()); //idee zum erzeugen der QR-Verteilungliste
+            tv_roomName.setText("Raumname: " + r.getName()); //set text of the TextViews
             tv_qrCodeR.setText("QR-Code Raum: " + r.getQrCode());
             tv_weaponName.setText("Waffenname: " + r.getWeaponList().get(0).getName());
             tv_qrCodeW.setText("QR-Code Waffe " + r.getWeaponList().get(0).getQrCode());
 
-            row.addView(tv_roomName);
+            row.addView(tv_roomName); //add all TextViews to row
             row.addView(tv_qrCodeR);
             row2.addView(tv_weaponName);
             row2.addView(tv_qrCodeW);
 
-            tbl.addView(row);
+            tbl.addView(row);   //add rows to table
             tbl.addView(row2);
         }
-        ll.addView(tbl);
+        ll.addView(tbl); //add table to Layout
 
         Button btn = new Button(this);
         btn.setText("Create Game");
