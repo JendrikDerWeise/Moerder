2dffb8fd64fe50844a659bd05c56b964160bd07d
JendrikDerWeise <jbulk@stud.hs-bremen.de>
2016-06-03 19:23:15 +0200

    Anonyme Anmeldung bei Firebase läuft

---
+++ MoerderCode/app/src/main/java/com/example/jendrik/moerder/GUI/MyApplication.java
@@ -0,0 +1,18 @@
+package com.example.jendrik.moerder.GUI;
+
+import android.content.Intent;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by Jendrik on 03.06.2016.
+ */
+public class MyApplication extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(this.getApplicationContext());
+
+    }
+}
--- MoerderCode/app/src/main/java/com/example/jendrik/moerder/GUI/Startscreen.java
+++ MoerderCode/app/src/main/java/com/example/jendrik/moerder/GUI/Startscreen.java
@@ -3,18 +3,27 @@ package com.example.jendrik.moerder.GUI;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
-
+import android.widget.Toast;
 import com.example.jendrik.moerder.GUI.Host.CreateGame;
 import com.example.jendrik.moerder.R;
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
 
 /**
  * Aufruf des Starbildschirms
  */
 public class Startscreen extends Activity {
-
+    Firebase myFirebaseRef = new Firebase("https://project-3462685560901461074.firebaseio.com/");
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,26 @@ public class Startscreen extends Activity {
         getActionBar().hide(); //macht das Ding oben weg
         setContentView(R.layout.activity_startscreen); //legt das Layout fest
 
-        FirebaseInstanceId.getInstance().getToken();
+        Log.d("Firebase Token ", FirebaseInstanceId.getInstance().getToken());
+        mAuth = FirebaseAuth.getInstance();
+
+        firebaseBindAuthStateListener();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+        signInAnonymously();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     public void onClickCreateGame(View button){
@@ -31,4 +59,46 @@ public class Startscreen extends Activity {
         startActivity(intent); //startet nächste Activity
     }
 
+    private void firebaseBindAuthStateListener(){
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("TAG", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("TAG", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+
+    private void signInAnonymously() {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("TAG", "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("TAG", "signInAnonymously", task.getException());
+                            Toast.makeText(Startscreen.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+
+    }
 }
