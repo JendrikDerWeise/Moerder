c5f0c11dc2691da1fec1143da5dd121063898e4c
queenPaM <rahelouise@yahoo.de>
2016-06-07 08:17:43 +0200

    design

--- MoerderCode/app/src/main/java/com/example/jendrik/moerder/GUI/Startscreen.java
+++ MoerderCode/app/src/main/java/com/example/jendrik/moerder/GUI/Startscreen.java
@@ -34,7 +34,7 @@ public class Startscreen extends Activity {
         getActionBar().hide(); //macht das Ding oben weg
         setContentView(R.layout.activity_startscreen); //legt das Layout fest
 
-        Log.d("Firebase Token ", FirebaseInstanceId.getInstance().getToken());
+//        Log.d("Firebase Token ", FirebaseInstanceId.getInstance().getToken());
         mAuth = FirebaseAuth.getInstance();
 
         firebaseBindAuthStateListener();
